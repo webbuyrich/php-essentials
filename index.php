6ed6c8d6f6b4d99737b356642392465fa02494bf
webbuyrich@gmail.com <webbuyrich@gmail.com>
2019-10-29 06:16:21 -0700

    shorter code to trim and check length

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 // $_POST['zipcode'] holds the value of the submitted form parameter "zipcode"
-$zipcode = trim($_POST['zipcode']);
 // Now zipcode holds that value, with any leading or trailing spaces removed
-$zip_length = strlen($zipcode);
 // Complain if the zip code length is not 5 characters long
-if($zip_length != 5){
+if(strlen(trim($_POST['zipcode'])) != 5){
     print 'Please enter a zip code that is 5 Characters long';
 }
 ?>
