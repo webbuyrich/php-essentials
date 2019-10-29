a814a8f853b7d0ed3abf689bb12ff16a5c66902a
webbuyrich@gmail.com <webbuyrich@gmail.com>
2019-10-29 07:02:13 -0700

    added formatting using printf

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
-if($_POST['email'] == 'johndoe@gmail.com'){
-    print 'Welcome John Doe';
-}
+// Format pricing with $%.2f
+$price = 5;
+$tax = 0.075;
+printf('The dish costs $%.2f', $price * (1 + $tax));
+echo '<br />';
 
-// Compare strings case insensitively
-if (strcasecmp($_POST['email'], 'johndoe@gmail.com') == 0 ){
-    print 'Welcome back John Doe';
-}
+// Zero padding with printf()
+$zip = '6520';
+$month = 2;
+$day = 6;
+$year  =  2019;
+printf("ZIP is %05d and the date is %02d/%02d/%d", $zip, $month, $day, $year);
 ?>
