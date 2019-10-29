9ad898b7dadb3ecf83c47c9ba21f220e5bdbb7a9
webbuyrich@gmail.com <webbuyrich@gmail.com>
2019-10-29 06:13:42 -0700

    trim and check length of variable

--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php
-
-//USE SQLite database 'dinner.db'
-$db = new PDO('sqlite:dinner.db');
-// Define what allowable meals are
-$meals = array('breakfast', 'lunch', 'dinner');
-//Check if submitted form parameter 'meal' is one of "breakfast", "lunch", or "dinner"
-$meal = isset($_POST['meal']);
-if (in_array($meal, $meals)){
-    // If so, get all fo the dishes for the specified meal
-    $stmt = $db->prepare('SELECT dish, price FROM meals WHERE meal LIKE ?');
-    $stmt->execute($meal);
-    $rows = $stmt->fetchAll();
-    // if no disheres were found in the database, say so
-    if(count($rows) == 0 ){
-        print "No dishes available";
-    } else {
-        // Print out each dish and its price as a row
-        // in an HTML Table
-        print '<table><tr><th>Dish</th><th>Price</th></tr>';
-        foreach($rows as $row){
-            print '<tr><td>$row[0]</td><td>$row[1]</td></tr>';
-        }
-        print '</table>';
-    }
-} else {
-    // This message will print if submitted parameter "meal" isnt in "breakfast", "lunch", or "dinner"
-    print "Unknown meal";
+// $_POST['zipcode'] holds the value of the submitted form parameter "zipcode"
+$zipcode = trim($_POST['zipcode']);
+// Now zipcode holds that value, with any leading or trailing spaces removed
+$zip_length = strlen($zipcode);
+// Complain if the zip code length is not 5 characters long
+if($zip_length != 5){
+    print 'Please enter a zip code that is 5 Characters long';
 }
 ?>
