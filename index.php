88447deedde90bd3361a5f663825b40e691d845f
webbuyrich@gmail.com <webbuyrich@gmail.com>
2019-10-29 05:56:49 -0700

    added isset to variable

--- index.php
+++ index.php
@@ -5,10 +5,11 @@ $db = new PDO('sqlite:dinner.db');
 // Define what allowable meals are
 $meals = array('breakfast', 'lunch', 'dinner');
 //Check if submitted form parameter 'meal' is one of "breakfast", "lunch", or "dinner"
-if (in_array($_POST['meal'], $meals)){
+$meal = isset($_POST['meal']);
+if (in_array($meal, $meals)){
     // If so, get all fo the dishes for the specified meal
     $stmt = $db->prepare('SELECT dish, price FROM meals WHERE meal LIKE ?');
-    $stmt->execute(array($_POST['meal']));
+    $stmt->execute($meal);
     $rows = $stmt->fetchAll();
     // if no disheres were found in the database, say so
     if(count($rows) == 0 ){
