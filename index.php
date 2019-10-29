54406b0e2a8a7f9f44e1947a1a1abaaaffdb5690
webbuyrich@gmail.com <webbuyrich@gmail.com>
2019-10-29 07:16:52 -0700

    combining numbers and strings

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
-// Format pricing with $%.2f
-$price = 5;
-$tax = 0.075;
-printf('The dish costs $%.2f', $price * (1 + $tax));
-echo '<br />';
+$price = 3.95;
+$tax_rate = 0.08;
+$tax_amount = $price * $tax_rate;
+$total_cost = $price + $tax_amount;
 
-// Zero padding with printf()
-$zip = '6520';
-$month = 2;
-$day = 6;
-$year  =  2019;
-printf("ZIP is %05d and the date is %02d/%02d/%d", $zip, $month, $day, $year);
+$username = 'james';
+$domain = '@example.com';
+$email_address = $username . $domain;
+
+print 'The tax is ' . $tax_amount;
+print "<br />"; //this prints a line break
+print 'The total cost is ' . $total_cost;
+print "<br />"; // this prints a line break
+echo $email_address;
+
+//Example 2.18 Combined assignment and addition
+// Add 3 the regular way
+$price = $price + 3;
+echo '<br /> price + 3 = ' . $price;
+
+// 3 with the combined operator
+$price += 3;
+echo '<br /> price + 6 = ' . $price; 
+
+// Example 2.19 Combined assignment and concatenation
+// Concatenate $domain to the end of $username the regular way
+$username = $username . $domain;
+echo '<br /> ' . $username;
+// Concatenate with combined operator
+$username2 = 'richard';
+$username2 .= $domain;
+echo '<br /> ' . $username2;
 ?>
